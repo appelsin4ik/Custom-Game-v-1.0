e6c514f66e3c662f5913695884655e1fc46e69b1
Kirill <Kirill@DESKTOP-SKBB3O8>
2024-10-16 17:15:01 +0200

    fgfgfgfgf

--- Custom_Game_Developing_v_1_0/src/main/Main.java
+++ Custom_Game_Developing_v_1_0/src/main/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		System.out.println("New Line added!");
 		System.out.println("New Line added!");
 		System.out.println("New Line added!");
+		System.out.println("New Line added!");
 	}
 
 }
