a8a45242245592f526b40b5c061ed66c14d2c185
s87159 <s87159@192.168.56.1>
2024-10-17 13:00:28 +0200

    Sys out YAARRRRR

--- Custom_Game_Developing_v_1_0/src/main/Main.java
+++ Custom_Game_Developing_v_1_0/src/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		System.out.println("New Line added!");
 		System.out.println("New Line added!");
 		System.out.println("New Line added!");
+		System.out.println("YARR");
 	}
 
 }
