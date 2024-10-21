898b3dd0c7df03b7047ecdb649332362dd4dddef
Kirill <Kirill@DESKTOP-SKBB3O8>
2024-10-21 17:23:13 +0200

    Sysout Kirill

--- Custom_Game_Developing_v_1_0/src/main/Main.java
+++ Custom_Game_Developing_v_1_0/src/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		System.out.println("New Line added!");
 		System.out.println("New Line added!");
 		System.out.println("New Line added!");
+		System.out.println("KIRILL");
 		System.out.println("JOOODIO");
 		System.out.println("YARR");
 		int i = 14;
